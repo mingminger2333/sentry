55121376d11f5f663eeb8dbf6d89f9a23cf57aa8
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-11-20 14:55:43 +0100

    parent 2a30ac698d80403fdb6d9b7b4a11dd2e45e9f902 author Priscila Oliveira <priscila.oliveira@sentry.io> 1605880543 +0100 committer Priscila Oliveira <priscila.oliveira@sentry.io> 1605889786 +0100
    feat(release-health): Add session term descriptions in release chart legends

--- src/sentry/static/sentry/app/views/releases/detail/overview/index.tsx
+++ src/sentry/static/sentry/app/views/releases/detail/overview/index.tsx
@@ -247,6 +247,7 @@ class ReleaseOverview extends AsyncView<Props> {
                         version={version}
                         hasDiscover={hasDiscover}
                         hasPerformance={hasPerformance}
+                        platform={project.platform}
                       />
                     )}
                     <Issues
